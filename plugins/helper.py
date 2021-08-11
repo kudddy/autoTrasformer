0bd04c05c4b6a523aff448c8adcb295d7499d09f
a16783869 <Zx0996750>
2021-08-11 12:24:12 +0300

    фикс ошибки с датами

--- plugins/helper.py
+++ plugins/helper.py
@@ -16,6 +16,18 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 
+def is_true(val):
+    if val:
+        return val
+
+
+def generate_city_str(citys_id: list):
+    city_str = ""
+    for city in citys_id:
+        city_str += "&city={}".format(city)
+    return city_str
+
+
 async def get_resp_from_yandex_systems(payload: dict):
     response = await requests.post(url_auto_ru,
                                    headers=headers_auto_ru,
@@ -51,9 +63,10 @@ async def get_search_res_yandex(text: str):
 
     year_from = data['suggests'][0]['originalParams'].get("year_from", False)
     year_to = data['suggests'][0]['originalParams'].get("year_to", False)
-
+    city_id = []
     if "regions" in data['suggests'][0]['view']:
-        city_id = data['suggests'][0]['view']['regions'][0]['id']
+        for region in data['suggests'][0]['view']['regions']:
+            city_id.append(region['id'])
     else:
         city_id = False
 
@@ -64,18 +77,22 @@ async def get_search_res_yandex(text: str):
 
     if model:
         model = marks.get(model, False)
-    # TODO подумать насчет того что в яндексе больше индексов
     if city_id:
-        city_id = citys.get(city_id, False)
+        city_id = [citys.get(x, False) for x in city_id]
+
+        city_id = list(filter(is_true, city_id))
+
+        if len(city_id) == 0:
+            city_id = False
 
     return mark, model, city_id, year_from, year_to
 
 
 async def get_cars_from_sberauto(brand_id: int or bool,
-                                 city_id: int or bool,
+                                 city_id: list or bool,
                                  model_id: int or bool,
-                                 year_to: int,
-                                 year_from: int):
+                                 year_to: int or bool,
+                                 year_from: int or bool):
     payload = {
         "filter": {
             "engine_type_code": [],
@@ -86,7 +103,7 @@ async def get_cars_from_sberauto(brand_id: int or bool,
             "label_code": [],
             "year_to": year_to if year_to else None,
             "year_from": year_from if year_from else None,
-            "city_id": [city_id] if city_id else [],
+            "city_id": city_id if city_id else [],
             "is_new": None,
             "catalog": [
                 {
@@ -134,33 +151,32 @@ def parse_response(resp: dict):
 
 
 def generate_url(brand_id: int,
-                 city_id: int,
+                 city_id: list,
                  model_id: int,
                  year_from: int,
                  year_to: int) -> str:
     # генерация ссылки относительно входящий параметов
-    # https: // sberauto.com / cars?brand = 229 = model = 2781 & city = 1 & dateTo = 2021 & dateFrom = 2018 & rental_car = exclude_rental
     if brand_id and city_id and model_id:
-        done_url = "https://sberauto.com/cars?brand={}=model={}&city={}&rental_car=exclude_rental".format(brand_id,
-                                                                                                          model_id,
-                                                                                                          city_id)
+        city_str = generate_city_str(city_id)
+        done_url = "https://sberauto.com/cars?brand={}=model={}&rental_car=exclude_rental{}".format(brand_id,
+                                                                                                    model_id,
+                                                                                                    city_str)
     elif brand_id and model_id:
         done_url = "https://sberauto.com/cars?brand={}=model={}&rental_car=exclude_rental".format(brand_id,
                                                                                                   model_id)
     elif brand_id and city_id:
+        city_str = generate_city_str(city_id)
         done_url = "https://sberauto.com/cars?brand={}&city={}&rental_car=exclude_rental".format(brand_id,
-                                                                                                 city_id)
-
+                                                                                                 city_str)
     elif brand_id:
         done_url = "https://sberauto.com/cars?brand={}&rental_car=exclude_rental".format(brand_id)
     else:
         done_url = "https://sberauto.com/cars?"
-
-    # TODO подумать над этим
     if year_to or year_from:
-        if year_to == year_from:
-            done_url = done_url + "&dateTo={}".format(year_from)
-        else:
-            done_url = done_url + "&dateTo={}&dateFrom={}".format(year_to, year_to)
-
+        if year_to and year_from:
+            done_url = done_url + "&dateTo={}&dateFrom={}".format(year_to, year_from)
+        elif year_to:
+            done_url = done_url + "&dateTo={}".format(year_to)
+        elif year_from:
+            done_url = done_url + "&dateFrom={}".format(year_from)
     return done_url
